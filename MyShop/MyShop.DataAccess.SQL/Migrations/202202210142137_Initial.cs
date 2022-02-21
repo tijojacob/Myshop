acf55db9baafe37b4949cfbeb478e09f89e6db80
Tijo Jacob <tijojosejacob@gmail.com>
2022-02-21 07:14:42 +0530

    Adding Entiry FrameWork

---
+++ MyShop/MyShop.DataAccess.SQL/Migrations/202202210142137_Initial.cs
@@ -0,0 +1,42 @@
+﻿namespace MyShop.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ProductCategories",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Catagery = c.String(nullable: false),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(nullable: false, maxLength: 30),
+                        Description = c.String(nullable: false),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Catagery = c.String(nullable: false),
+                        Image = c.String(),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+            DropTable("dbo.ProductCategories");
+        }
+    }
+}
